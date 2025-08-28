d55d83f4c52325c69bec6f039a3cef17ac545559
CunxiaoNi <cunxiaoni@gmail.com>
2025-08-28 17:19:43 +0800

    [Dev] Implements softmax top-k using Tilelang
    This commit implements a softmax top-k operation using Tilelang.
    
    The implementation includes a Tilelang kernel for calculating top-k gates and indices, along with necessary fragment buffer allocations and data transfers. It also includes autotuning configurations.
    
    The changes also modify the main function to test the new kernel and profile its performance.
    Additionally, converts topk_indices to int32 to ensure compatibility.
    
    The test file also includes autotuning configurations.

--- MoE_develop/topk/softmax_topk_tilelang.py
+++ MoE_develop/topk/softmax_topk_tilelang.py
@@ -1,132 +1,137 @@
+
 import tilelang
 import tilelang.language as T
 import torch.nn.functional as F
+import torch
+from tilelang.engine.callback import register_cuda_postproc_callback
+import itertools
+
 
 # softmax + topk
 # logits (num_token, num_expert) -> softmax_logits (num_token, num_expert), dtype=float32
 # top_k_gates (num_token, top_k), dtype=float32
 # top_k_indices (num_token, top_k), dtype=int32
-@tilelang.jit
-def topk_gate(
+
+# tilelang.disable_cache()
+
+# torch.manual_seed(42)   
+
+def get_configs():
+    iter_params = dict(
+        block_tokens=[64, 128, 256],
+        threads=[128, 256, 512],
+    )
+    return [dict(zip(iter_params, values)) for values in itertools.product(*iter_params.values())]
+
+
+@tilelang.autotune(configs=get_configs())
+@tilelang.jit(out_idx=[1, 2])
+def topk_softmax(
     num_tokens,
-    num_expert,
-    top_k,
-    block_M,
-    block_N,
-    block_K,
-    stages=1,
+    num_experts,
+    topk,
+    block_tokens,
     threads=128,
 ):
     dtype = "float32"
     accum_dtype = "float32"
-    scale = (1.0 / num_expert) ** 0.5 * 1.44269504 # log2(e)
+    scale = 1.44269504 #log2(e)
 
-    
-    @T.macro
-    def Softmax(
-            acc_s: T.FragmentBuffer([block_M, block_N], accum_dtype),
-            acc_s_cast: T.FragmentBuffer([block_M, block_N], dtype),
-    ):
-        max_val = T.alloc_fragment([block_M], accum_dtype)
-        scores_sum = T.alloc_fragment([block_M], accum_dtype)
-
-        T.fill(max_val, -T.infinity(accum_dtype))
-        T.reduce_max(acc_s, max_val, dim=1, clear=False)
-
-        for i, j in T.Parallel(block_M, block_N):
-            acc_s[i, j] = T.exp2(acc_s[i, j] * scale - max_val[i] * scale)
-
-        T.reduce_sum(acc_s, scores_sum, dim=1, clear=False)
-
-        for i in T.Parallel(block_M):
-            acc_s[i, j] = acc_s[i, j] / scores_sum[i]
-        T.copy(acc_s, acc_s_cast)
-    
-    @T.macro
-    def TopK(
-        logits: T.Tensor([num_tokens, num_expert], dtype),
-        top_k_gates: T.Tensor([num_tokens, top_k], dtype),
-        top_k_indices: T.Tensor([num_tokens, top_k], dtype),
-    ):
-        with T.Kernel(T.ceildiv(num_tokens, block_M), T.ceildiv(num_expert, block_N)) as (by, bx):
-            cur_vals = T.alloc_fragment([block_M, block_N], dtype)
-            candidate_idx = T.alloc_fragment([block_M, block_N], "int32")
-
-            top_k_gates_local = T.alloc_fragment([block_M, top_k], dtype)
-            top_k_indices_local = T.alloc_fragment([block_M, top_k], "int32")
-
-            max_val = T.alloc_fragment([block_M], dtype)
-            max_idx = T.alloc_fragment([block_M], "int32")
-            T.copy(logits, cur_vals)
-            
-            for k in T.Parallel(top_k):
-                T.fill(max_val, -T.infinity(dtype))
-                T.reduce_max(cur_vals, max_val, dim=1, clear=False)
-
-                for i, j in T.Parallel(block_M, block_N):
-                    candidate_idx[i, j] = T.if_then_else(
-                        cur_vals[i, j] == max_val[i],
-                        bx * block_N + j,
-                        num_expert
-                    )
-
-                T.fill(max_idx, 128)
-                T.reduce_min(candidate_idx, max_idx, dim=1, clear=False)
-                
-                for i in T.Parallel(block_M):
-                    top_k_gates_local[i, k] = max_val[i]
-                    top_k_indices_local[i, k] = max_idx[i]
-
-                    for j in T.Parallel(block_N):
-                        cur_vals[i, j] = T.if_then_else(
-                            j + (bx * block_N) == max_idx[i],
-                            -T.infinity(dtype),
-                            cur_vals[i, j]
-                        )
-                
-            T.copy(top_k_gates_local, top_k_gates[by * block_M, 0])
-            T.copy(top_k_indices_local, top_k_indices[by * block_M, 0])
-                
-    
-
-            
-    
     @T.prim_func
-    def softmax_topk(
-        logits: T.Tensor([num_tokens, num_expert], dtype),
-        top_k_gates: T.Tensor([num_tokens, top_k], dtype),
-        top_k_indices: T.Tensor([num_tokens, top_k], dtype),
+    def topk_softmax_kernel(
+        logits: T.Tensor([num_tokens, num_experts], dtype),
+        topk_gates: T.Tensor([num_tokens, topk], dtype),
+        topk_indices: T.Tensor([num_tokens, topk], "int32"),
     ):
-        with T.Kernel(T.ceildiv(num_tokens, block_M), T.ceildiv(num_expert, block_N)) as (by, bx):
-            pass
-                        
+        with T.Kernel(T.ceildiv(num_tokens, block_tokens), threads=threads) as bx:
+            logits_frag = T.alloc_fragment([block_tokens, num_experts], dtype=dtype)
+            max_val = T.alloc_fragment([block_tokens], dtype=dtype)
+            expand_max_idx = T.alloc_fragment([block_tokens, num_experts], "int32")
+            max_idx = T.alloc_fragment([block_tokens], "int32")
+            gates_frag = T.alloc_fragment([block_tokens, topk], dtype=dtype)
+            
+            T.copy(logits[bx * block_tokens, 0], logits_frag)
 
+            for k in T.serial(topk):
+                T.fill(expand_max_idx, -1)
+                T.reduce_max(logits_frag, max_val, dim=1, clear=True)
                 
 
-
-
+                for i, j in T.Parallel(block_tokens, num_experts):
+                    expand_max_idx[i, j] = T.if_then_else(max_val[i] == logits_frag[i, j], j, expand_max_idx[i, j])
+                
+                T.reduce_max(expand_max_idx, max_idx, dim=1, clear=True)
+                
+                for i, j in T.Parallel(block_tokens, num_experts):
+                    logits_frag[i, j] = T.if_then_else(max_val[i] == logits_frag[i, j], -10000.0, logits_frag[i, j])
+                
+                for i in T.Parallel(block_tokens):
+                    # topk_gates[bx * block_tokens + i, k] = max_val[i]
+                    gates_frag[i, k] = max_val[i]
+                    topk_indices[bx * block_tokens + i, k] = max_idx[i]
             
+            # 对gates_frag进行softmax
+            exp_max = T.alloc_fragment([block_tokens], dtype)
+            scores_sum = T.alloc_fragment([block_tokens], dtype)
+            T.fill(exp_max, -T.infinity(dtype))
+
+            T.reduce_max(gates_frag, exp_max, dim=1, clear=True)
+
+            for i, j in T.Parallel(block_tokens, topk):
+                gates_frag[i, j] = T.exp2(gates_frag[i, j] * scale - exp_max[i] * scale)
+            
+            T.fill(scores_sum, 0.0)
+
+            T.reduce_sum(gates_frag, scores_sum, dim=1, clear=False)
+
+            for i, j in T.Parallel(block_tokens, topk):
+                gates_frag[i, j] = gates_frag[i, j] / scores_sum[i]
+            
+            for k in T.serial(topk):
+                for i in T.Parallel(block_tokens):
+                    topk_gates[bx * block_tokens + i, k] = gates_frag[i, k]
+    
+    return topk_softmax_kernel
+        
+
 def ref_program(logits, top_k):
 
-    logits = F.softmax(logits)
     top_k_gates, top_k_indices = logits.topk(top_k, dim=1)
+    top_k_gates = F.softmax(top_k_gates)
 
-    return top_k_gates, top_k_indices
+    return top_k_gates, top_k_indices.to(torch.int32)
 
 
 def main():
-    num_tokens, num_expert, top_k = 320, 128, 6
-    block_M, block_N, block_K = 128, 64, 128
-    kernel = topk_gate(num_tokens, num_expert, top_k, block_M, block_N, block_K)
-    profiler = kernel.get_profiler(tensor_supply_type=tilelang.TensorSupplyType.Normal)
-    profiler.assert_allclose(ref_program, rtol=0.01, atol=0.01)
-    print("All checks passed!")
+    num_tokens = 320
+    num_experts = 128
+    top_k = 6
+    block_tokens = 64
 
-    latency = profiler.do_bench(ref_program, warmup=500)
-    print("ref_program: {:.2f} ms".format(latency))
+    logits = torch.rand(num_tokens, num_experts).to("cuda")
 
-    latency = profiler.do_bench(n_warmup=10, n_repeat=10)
-    print("tilelang: {:.4f} ms".format(latency))
+    # kernel = topk_softmax(num_tokens, num_experts, top_k, block_tokens)
+    kernel = topk_softmax(num_tokens=num_tokens, num_experts=num_experts, topk=top_k)
+    tl_gates, tl_indices = kernel(logits)
+    # print(f"tl_gates:")
+    # print(tl_gates)
+    
+    # print(kernel.get_kernel_source())
+    print(kernel.config)
+
+    torch_gates, torch_indices = ref_program(logits, top_k)
+    # print(f"torch_gates:")
+    # print(torch_gates)
+    
+    # test accuracy
+    torch.testing.assert_close(tl_gates, torch_gates)
+    torch.testing.assert_close(tl_indices, torch_indices)
+
+    # profile
+    profiler = kernel.get_profiler(tensor_supply_type=tilelang.TensorSupplyType.Auto)
+    tilelang_latency = profiler.do_bench()
+    print(f"Tilelang latency: {tilelang_latency}")
+
     
 if __name__ == "__main__":
     main()
